38a9151744689d9beb4a370f904e041c7861ad57
vaishnavisawant57 <80047521+vaishnavisawant57@users.noreply.github.com>
2022-04-08 23:13:35 +0530

    Add files via upload

---
+++ lib/widgets/joinButton.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+class JoinButton extends StatelessWidget {
+  final Function()? function;
+  final Color backgroundColor;
+  final Color borderColor;
+  final String text;
+  final Color textColor;
+  const JoinButton({Key? key, this.function, required this.backgroundColor, required this.borderColor, required this.text, required this.textColor}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.only(top: 2),
+      child: TextButton(
+        onPressed: function,
+        child: Container(
+          decoration: BoxDecoration(
+            color: backgroundColor,
+            border: Border.all(
+              color: borderColor,
+            ),
+            borderRadius: BorderRadius.circular(50),
+          ),
+          alignment: Alignment.center,
+          child: Text(
+            text,
+            style: TextStyle(
+              color: textColor,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          width: 70,
+          height: 27,
+        ),
+      ),
+    );
+  }
+}
